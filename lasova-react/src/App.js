862b97a27a29a34fbacfafd9e6598d5f3a8c8212
Noam <63012023+noam-y@users.noreply.github.com>
2022-01-10 00:34:38 +0200

    removing some default files...

--- lasova-react/src/App.js
+++ lasova-react/src/App.js
@@ -1,33 +1,10 @@
-import React, { useState } from "react"
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import offline_data from './mock-data.json';
 
+const App = () => {
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-const app = () => {
-
-  const [volunteers, setvolunteers] = useState(offline_data);
+  //const [volunteers, setvolunteers] = useState(offline_data);
   return <div className="basic-table-div">
     <div className="person-row">
       {volunteers.map((volunteer) => (
