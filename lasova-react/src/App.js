e096fd9c990c055e266680f9c1f03be09126a195
Noam <63012023+noam-y@users.noreply.github.com>
2022-01-10 00:13:55 +0200

    adding basic excel table (no use for it now), a json version of it, and a very basic func to return names of volunteers from given table. this code was not tested so beware

--- lasova-react/src/App.js
+++ lasova-react/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import offline_data from './mock-data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+const app = () => {
+
+  const [volunteers, setvolunteers] = useState(offline_data);
+  return <div className="basic-table-div">
+    <div className="person-row">
+      {volunteers.map((volunteer) => (
+        <h1>
+          {volunteer.first_name} {volunteer.last_name}
+        </h1>
+      ))}
     </div>
-  );
+
+  </div>
 }
 
 export default App;
