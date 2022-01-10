a01b824264444df2e8f71fdc64108ca1a8fd9b93
Noam <63012023+noam-y@users.noreply.github.com>
2022-01-10 10:30:42 +0200

    this iterates throughout the basic volunteers json, without any front end work done.

--- lasova-react/src/App.js
+++ lasova-react/src/App.js
@@ -1,17 +1,19 @@
-//import React, { useState } from "react";
+import React, { useState } from "react";
 import offline_data from './mock-data.json';
-const React = require("react")
-const { useState } = require("react")
 //import "./App.css";
 
 const App = () => {
   const [volunteers, setvolunteers] = useState(offline_data);
   return <div className="basic-table-div">
-    <div className="person-row">
+    <div className="all-volunteers">
       {volunteers.map((volunteer) => (
-        <h1>
-          {volunteer.first_name} {volunteer.last_name}
-        </h1>
+        <div className="volunteer-view">
+          {volunteer.first_name} {volunteer.last_name} <br />
+          {volunteer.taz} <br /><br />  
+          {/* inside this div you can create a view of a single volunteer. 
+          I didnt choose a tableview since im not sure whether Yulia would love another way
+          displaying the information. :) */}
+        </div>
       ))}
     </div>
 
