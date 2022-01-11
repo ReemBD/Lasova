13d6c9e89405677e4126b5773c56a6ed92a36b1e
Noam <63012023+noam-y@users.noreply.github.com>
2022-01-11 23:54:05 +0200

    basic function to read json from server

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
           <div className="volunteer-view">
             {volunteer.first_name} {volunteer.last_name} <br />
             {volunteer.taz} <br />
+            {readJson()}
             <br />
             {/* inside this div you can create a view of a single volunteer. 
           I didnt choose a tableview since im not sure whether Yulia would love another way
@@ -24,5 +25,17 @@ const App = () => {
     </div>
   );
 };
+
+
+function readJson () {
+  console.log(this)
+  // http://localhost:8080
+  fetch('http://localhost:8000/users').then((response) => response.json()).then(json => {
+      vm.users = json
+      console.log(vm.users)
+  }).catch(function () {
+      vm.dataError = true
+  })
+}
 
 export default App;
