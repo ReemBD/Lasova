3258af12b8a6d17efa66fccdeee150c20471e285
saharc8 <saharc8@gmail.com>
2022-01-16 23:18:06 +0200

    activate search by first_name

--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -5,58 +5,64 @@ const initialStore = {
 };
 
 const reducer = (state = initialStore, action) => {
-  if (action.type === "CLEAR_CART") {
-    return { ...state, cart: [] };
+  if (action.type === "SEARCH") {
+    let newVolunteers = [...offline_data];
+    newVolunteers = newVolunteers.filter((volunteer) => {
+      return volunteer.first_name
+        .toLowerCase()
+        .includes(action.payload.toLowerCase());
+    });
+    return { ...state, volunteers: newVolunteers };
   }
-  if (action.type === "REMOVE") {
-    return {
-      ...state,
-      cart: state.cart.filter((p) => p.id !== action.payload),
-    };
-  }
-  if (action.type === "INCREASE") {
-    return {
-      ...state,
-      cart: state.cart.map((p) => {
-        if (p.id === action.payload) {
-          return { ...p, amount: p.amount + 1 };
-        } else return p;
-      }),
-    };
-  }
-  if (action.type === "DECREASE") {
-    return {
-      ...state,
-      cart: state.cart.map((p) => {
-        if (p.id === action.payload) {
-          if (p.amount > 0) {
-            return { ...p, amount: p.amount - 1 };
-          } else return { ...p, amount: 0 };
-        } else return p;
-      }),
-    };
-  }
-  if (action.type === "TOTAL") {
-    let { amount, total } = state.cart.reduce(
-      (cartTotal, cartItem) => {
-        const { price, amount } = cartItem;
-        const itemTotal = price * amount;
-        cartTotal.amount += amount;
-        cartTotal.total += itemTotal;
-        return cartTotal;
-      },
-      {
-        amount: 0,
-        total: 0,
-      }
-    );
-    total = total.toFixed(2);
-    return {
-      ...state,
-      amount,
-      total,
-    };
-  }
+  // if (action.type === "REMOVE") {
+  //   return {
+  //     ...state,
+  //     cart: state.cart.filter((p) => p.id !== action.payload),
+  //   };
+  // }
+  // if (action.type === "INCREASE") {
+  //   return {
+  //     ...state,
+  //     cart: state.cart.map((p) => {
+  //       if (p.id === action.payload) {
+  //         return { ...p, amount: p.amount + 1 };
+  //       } else return p;
+  //     }),
+  //   };
+  // }
+  // if (action.type === "DECREASE") {
+  //   return {
+  //     ...state,
+  //     cart: state.cart.map((p) => {
+  //       if (p.id === action.payload) {
+  //         if (p.amount > 0) {
+  //           return { ...p, amount: p.amount - 1 };
+  //         } else return { ...p, amount: 0 };
+  //       } else return p;
+  //     }),
+  //   };
+  // }
+  // if (action.type === "TOTAL") {
+  //   let { amount, total } = state.cart.reduce(
+  //     (cartTotal, cartItem) => {
+  //       const { price, amount } = cartItem;
+  //       const itemTotal = price * amount;
+  //       cartTotal.amount += amount;
+  //       cartTotal.total += itemTotal;
+  //       return cartTotal;
+  //     },
+  //     {
+  //       amount: 0,
+  //       total: 0,
+  //     }
+  //   );
+  //   total = total.toFixed(2);
+  //   return {
+  //     ...state,
+  //     amount,
+  //     total,
+  //   };
+  // }
   return state;
 };
 
--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 // Libraries installed: express, nodemon, dotenv
 // temporarily libraries: fs
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // requests can only come from this domains
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENTS_DOMAIN,
-}))
+  })
+);
 
 // Settings up routes, can be found at './routes/'
-app.use('/users', require('./routes/users/get.js'));
+app.use("/users", require("./routes/users/get.js"));
 
 // Starting the server on http://localhost:PORT
-app.listen(process.env.PORT, () => console.log(`Server Is Up!\nhttp://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server Is Up!\nhttp://localhost:${process.env.PORT}`)
+);
