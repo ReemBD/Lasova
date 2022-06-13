c5cec48d40b1d8c9a89fd457b3f13928f2bed560
Reem Ben-David <reemile1996@gmail.com>
2022-06-13 19:04:26 +0300

    Mongo atlas integration Groups - Added group schema/model

---
+++ api/group/group.schema.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const Group = new Schema({
+  type: { type: String, default: '', trim: true },
+  name: { type: String, default: '', trim: true },
+  contactName: { type: String, default: '', trim: true },
+  contactRole: { type: String, default: '', trim: true },
+  cellphone: { type: String, default: '', trim: true },
+  volunteersCount: { type: Number, default: 0 },
+  reportedHours: { type: Number, default: 0 },
+  volunteeringsCount: { type: Number, default: 0 },
+});
+
+module.exports = new model('Group', Group);
