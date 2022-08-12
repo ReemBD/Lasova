29ff4fdb009eeaa6748d83a34779a5f4c9a2bcf4
Amit Bar-Gil <amit217@yandex.com>
2022-08-12 16:10:36 +0300

    Group modal first block unly UI

--- client/src/components/NewGroup.jsx
+++ client/src/components/NewGroup.jsx
@@ -17,6 +17,46 @@ function NewGroupModal({ open, setOpen }) {
                     type='button'
                 />
                 <h1 className='new_group_title'>הוספת קבוצה/ארגון חדשים</h1>
+                <form className='new_group_form'>
+                    <div className='new_group_organization-data'>
+                        <div>
+                            <h2 className='new_group_organization-title'>
+                                ארגון
+                            </h2>
+                            <div className='new_group_organization-form'>
+                                <label htmlFor='name'>
+                                    <input
+                                        className='input'
+                                        type='text'
+                                        placeholder='שם'
+                                        name='name'
+                                    />
+                                </label>
+                                <select name='profile' className='input'>
+                                    <option id='0' value='null'>
+                                        פרופיל
+                                    </option>
+                                    <option id='1' value='עצמאי'>
+                                        עצמאי
+                                    </option>
+                                    <option id='2' value='סטודנט'>
+                                        סטודנט
+                                    </option>
+                                    <option id='3' value='שלצ'>
+                                        של"צ
+                                    </option>
+                                </select>
+                                <label htmlFor='city'>
+                                    <input
+                                        className='input'
+                                        placeholder='עיר'
+                                        type='text'
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </Box>
         </Modal>
     );
