f5bd4b8c838620141b375f45301f6f6ecc8439dd
Amit Bar-Gil <amit217@yandex.com>
2022-08-15 19:35:35 +0300

    Delete merge

--- client/src/components/BaseTable.jsx
+++ client/src/components/BaseTable.jsx
@@ -8,86 +8,85 @@ import CustomNoRowsOverlay from "./dataGrid/CustomNoRowsOverlay";
 import TableLoader from "./dataGrid/TableLoader";
 
 const BaseTable = ({
-    entities,
-    columns,
-    exportRef,
-    csvBtnRef,
-    exportFileName,
-    onEntityClick,
-    activeFilter,
-    dropdownPosition,
-    filterOptions,
-    onSetFilter,
-    filter,
+  entities,
+  columns,
+  exportRef,
+  csvBtnRef,
+  exportFileName,
+  onEntityClick,
+  activeFilter,
+  dropdownPosition,
+  filterOptions,
+  onSetFilter,
+  filter,
 }) => {
-    const [rows, setRows] = useState(entities || []);
+  const [rows, setRows] = useState(entities || []);
 
-    useEffect(() => {
-        exportRef.current = () => csvBtnRef.current.click();
-    }, [csvBtnRef, exportRef]);
+  useEffect(() => {
+    exportRef.current = () => csvBtnRef.current.click();
+  }, []);
 
-    useEffect(() => {
-        if (entities !== null) {
-            let entitiesToShow = entities;
-            for (let filterBy in filter) {
-                const currFilter = filter[filterBy];
-                if (currFilter) {
-                    if (currFilter !== "בחר הכל") {
-                        entitiesToShow = entitiesToShow.filter(
-                            (val) => val[filterBy] === currFilter
-                        );
-                    }
-                }
-            }
-            setRows(entitiesToShow);
+  useEffect(() => {
+    if (entities !== null) {
+      let entitiesToShow = entities;
+      for (let filterBy in filter) {
+        const currFilter = filter[filterBy];
+        if (currFilter) {
+          if (currFilter !== "בחר הכל") {
+            entitiesToShow = entitiesToShow.filter(
+              (val) => val[filterBy] === currFilter
+            );
+          }
         }
-    }, [entities, filter]);
+      }
+      setRows(entitiesToShow);
+    }
+  }, [entities, filter]);
 
-    return (
-        <>
-            {activeFilter && (
-                <div
-                    className='filter-menu'
-                    style={{
-                        ...dropdownPosition,
-                    }}>
-                    {filterOptions[activeFilter].map((o) => (
-                        <MenuItem
-                            className='filter-option'
-                            key={o}
-                            onClick={() => onSetFilter(o)}>
-                            {o}
-                        </MenuItem>
-                    ))}
-                </div>
-            )}
-            <section className='base-table'>
-                <DataGrid
-                    rows={rows}
-                    getRowId={(row) => row._id}
-                    columns={columns}
-                    components={{
-                        Toolbar: () => (
-                            <ExportCsvBtn
-                                name={exportFileName}
-                                csvBtnRef={csvBtnRef}
-                            />
-                        ),
-                        LoadingOverlay: () => <TableLoader />,
-                        NoRowsOverlay: () => <CustomNoRowsOverlay />,
-                    }}
-                    loading={!rows}
-                    hideFooter
-                    checkboxSelection
-                    disableColumnMenu
-                    disableSelectionOnClick
-                    onRowDoubleClick={(ev) => {
-                        onEntityClick(ev.row);
-                    }}
-                />
-            </section>
-        </>
-    );
+  return (
+    <>
+      {activeFilter && (
+        <div
+          className="filter-menu"
+          style={{
+            ...dropdownPosition,
+          }}
+        >
+          {filterOptions[activeFilter].map((o) => (
+            <MenuItem
+              className="filter-option"
+              key={o}
+              onClick={() => onSetFilter(o)}
+            >
+              {o}
+            </MenuItem>
+          ))}
+        </div>
+      )}
+      <section className="base-table">
+        <DataGrid
+          rows={rows}
+          getRowId={(row) => row._id}
+          columns={columns}
+          components={{
+            Toolbar: () => (
+              <ExportCsvBtn name={exportFileName} csvBtnRef={csvBtnRef} />
+            ),
+            LoadingOverlay: () => <TableLoader />,
+            NoRowsOverlay: () => <CustomNoRowsOverlay />,
+          }}
+          loading={!rows}
+          hideFooter
+          checkboxSelection
+          disableColumnMenu
+          disableSelectionOnClick
+          onRowDoubleClick={(ev) => {
+            onEntityClick(ev.row);
+          }}
+        />
+      </section>
+    </>
+  );
 };
 
 export default BaseTable;
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
-const fileUpload = require("express-fileupload");
-const mongoose = require("mongoose");
-const { apiAccessAllowedURIs } = require("./env/index.config");
+const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
 const {
-    authenticateToken,
-} = require("./middlewares/authentication.middleware");
+  env,
+  dbURI,
+  dbName,
+  apiAccessAllowedURIs,
+} = require('./env/index.config');
 
 // requests can only come from this domains
-if (process.env.NODE_ENV === "development") {
-    app.use(
-        cors({
-            origin: process.env.API_ACCESS_ALLOWED_URIs.split(","),
-        })
-    );
+if (env === 'development') {
+  app.use(
+    cors({
+      origin: apiAccessAllowedURIs,
+    })
+  );
 } else {
-    app.use(express.static(path.resolve(__dirname, "public")));
+  app.use(express.static(path.resolve(__dirname, 'public')));
 }
 
 // Enables parsing of json req bodies.
@@ -29,26 +31,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 
 // connect to db
-mongoose.connect(process.env.DB_URI, {
-    dbName: process.env.DB_NAME,
-    useUnifiedTopology: true,
+mongoose.connect(dbURI, {
+  dbName,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Succesfully connected to db", db.db.databaseName);
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Succesfully connected to db', db.db.databaseName);
 });
 
-app.use("/api/volunteer", require("./api/volunteer/volunteer.routes"));
-app.use("/api/group", require("./api/group/group.routes"));
-app.use("/api/auth", require("./api/auth/auth.routes"));
-app.use("/api/user", require("./api/user/user.routes"));
-
-app.get("/**", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.use('/api/volunteer', require('./api/volunteer/volunteer.routes'));
+app.use('/api/group', require('./api/group/group.routes'));
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Starting the server on http://localhost:PORT
 app.listen(process.env.PORT, () =>
-    console.log(`Server Is Up!\nhttp://localhost:${process.env.PORT}`)
+  console.log(`Server Is Up!\nhttp://localhost:${process.env.PORT}`)
 );
