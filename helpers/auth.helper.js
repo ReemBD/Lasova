d8f5e1dc91fe6f4b548385ae2cba4bfd09f72352
Reem Ben-David <reemile1996@gmail.com>
2022-08-19 12:17:33 +0300

    git

---
+++ helpers/auth.helper.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+
+const verifyAuthToken = (token) => {
+  const decoded = jwt.verify(token, process.env.LASOVA_ACCESS_TOKEN_SECRET);
+  return decoded || false;
+};
+
+const validateEmail = (email) => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+module.exports = {
+  verifyAuthToken,
+  validateEmail,
+};
